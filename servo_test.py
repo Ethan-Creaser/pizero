c6aa8b4fc95ee3125685a4775ef3d7040839f5c7
Ethan-Creaser <100246761+Ethan-Creaser@users.noreply.github.com>
2024-02-24 15:22:02 +1100

    NA

--- servo_test.py
+++ servo_test.py
@@ -1,9 +1,12 @@
 import pigpio
+from gpiozero import LED
 import time
 
 SERVO_PIN = 13
+led = LED(26)
 
 pi = pigpio.pi()
+
 print("Hardware revision ", pi.get_hardware_revision())
 print("PIGPIO Version ", pi.get_pigpio_version())
 
@@ -19,12 +22,16 @@ try:
         # Set the servo to 0 degrees
         set_servo_angle(0)
         time.sleep(1)  # Wait 1 second
+        led.toggle()
         # Set the servo to 90 degrees
         set_servo_angle(90)
         time.sleep(1)  # Wait 1 second
+        led.toggle()
         # Set the servo to 180 degrees
         set_servo_angle(180)
         time.sleep(1)  # Wait 1 second
+        led.toggle()
+
 except KeyboardInterrupt:
     # Turn off the servo on Ctrl+C
     pi.set_servo_pulsewidth(SERVO_PIN, 0)
