0d0c0a451ae2b33bbf13ab0b78ba10c7fb95584f
Ethan-Creaser <100246761+Ethan-Creaser@users.noreply.github.com>
2024-02-24 15:00:56 +1100

    NA

--- servo_test.py
+++ servo_test.py
@@ -5,6 +5,8 @@ servo = 13
 
 pwm = pigpio.pi()
 pwm.set_mode(servo, pigpio.OUTPUT)
+print("Hardware revision ",pi.get_hardware_revision())
+print("PIGPIO Version ",pi.get_pigpio_version())
 
 pwm.set_PWM_frequency(servo, 50)
 pwm.set_PWM_range(servo, 20000)  # 1,000,000 / 50 = 20,000us for 100% duty cycle
