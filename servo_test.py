38f55bc3b4b2f34911bd06a6695f7f4bb9473428
Ethan-Creaser <100246761+Ethan-Creaser@users.noreply.github.com>
2024-02-24 14:42:39 +1100

    Update servo_test.py

--- servo_test.py
+++ servo_test.py
@@ -9,20 +9,15 @@ def set_servo_angle(angle):
     pulse_width = int(angle * 2000 / 180) + 500
     # Set the servo pulse width
 
-try:
-    while True:
+ while True:
         # Set the servo to 0 degrees
-        servo.min()
-        time.sleep(1)  # Wait 1 second
-        # Set the servo to 90 degrees
-        servo.max()
-        time.sleep(1)  # Wait 1 second
-        # Set the servo to 180 degrees
-        servo.mid()
-        time.sleep(1)  # Wait 1 second
+    servo.min()
+    time.sleep(1)  # Wait 1 second
+    # Set the servo to 90 degrees
+    servo.max()
+    time.sleep(1)  # Wait 1 second
+    # Set the servo to 180 degrees
+    servo.mid()
+    time.sleep(1)  # Wait 1 second
         
-except KeyboardInterrupt():
-    # Turn off the servo on Ctrl+C
-    servo.set_servo_pulsewidth(SERVO_PIN, 0)
-    # Disconnect from the pigpio daemon
-    pi.stop()
+
