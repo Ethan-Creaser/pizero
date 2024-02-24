0e8551a4f4233f5f6e096fcf6776f0643a2f751d
Ethan-Creaser <100246761+Ethan-Creaser@users.noreply.github.com>
2024-02-24 15:54:32 +1100

    NA

--- servo_test.py
+++ servo_test.py
@@ -20,7 +20,7 @@ def set_servo_angle(angle):
 
 try:
     angle = 0
-    set_servo_angle(180)
+    #set_servo_angle(180)
     pi.set_servo_pulsewidth(SERVO_PIN, 2000)
     for i in range(4):
         angle += 45
