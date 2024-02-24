862bbad9f8da415e53c865d7d15ae2464aeab51f
Ethan-Creaser <100246761+Ethan-Creaser@users.noreply.github.com>
2024-02-24 16:15:44 +1100

    NA

--- servo_test.py
+++ servo_test.py
@@ -21,7 +21,7 @@ def set_servo_angle(angle):
 try:
     angle = 0
     #set_servo_angle(180)
-    pi.set_servo_pulsewidth(SERVO_PIN, 1800)
+    pi.set_servo_pulsewidth(SERVO_PIN, 2000)
     time.sleep(2)
     pi.set_servo_pulsewidth(SERVO_PIN, 700)
     time.sleep(2)
